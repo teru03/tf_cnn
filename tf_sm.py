3df0c6b166ae0dd75a4f17254dbf898f70f945a5
teruo morimoto <terusun@gmail.com>
2017-11-14 14:37:22 +0000

    add dropout

--- tf_sm.py
+++ tf_sm.py
@@ -10,9 +10,10 @@ from sklearn import preprocessing
 import argparse
 import csv
 import os
+from fbas import FeatureBinarizatorAndScaler
 
 # Parameters
-learning_rate = 0.03 # 学習率 高いとcostの収束が早まる
+learning_rate = 0.003 # 学習率 高いとcostの収束が早まる
 batch_size = 5000     # 学習1回ごと( sess.run()ごと )に訓練データをいくつ利用するか
 display_step = 1     # 1なら毎エポックごとにcostを表示
 train_size = 400000     # 全データの中でいくつ訓練データに回すか
@@ -24,6 +25,8 @@ n_hidden_2 = 128     # 隠れ層2のユニットの数
 n_hidden_3 = 64      # 隠れ層3のユニットの数
 n_classes = 2        # 分類するクラスの数 今回は生き残ったか否かなので2
 
+prob = 0.7
+
 usecolumns = []
 
 def model_softmax(x, n_input):
@@ -32,11 +35,11 @@ def model_softmax(x, n_input):
     return tf.add(tf.matmul(x, w), b)
 
 # Create model
-def multilayer_perceptron(x, n_input):
+def multilayer_perceptron(x, n_input, keep_prob):
     # Initializing the variables
     weights = {
         'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-        'h2': tf.Variable(tf.random_normal([n_input, n_hidden_2])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
         'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
         'out': tf.Variable(tf.random_normal([n_hidden_3, n_classes]))
     }
@@ -50,74 +53,66 @@ def multilayer_perceptron(x, n_input):
 
     # Store layers weight & bias
     # Hidden layer with RELU activation
-    #layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    #layer_1 = tf.nn.relu(layer_1)
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
     #layer_1 = tf.nn.sigmoid(layer_1)
-    #layer_1 = tf.nn.softmax(layer_1)
+    do_1 = tf.nn.dropout(layer_1, keep_prob)
 
     ##layer_1 = tf.nn.softmax(layer_1)
     ## Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(x, weights['h2']), biases['b2'])
-    ##layer_2 = tf.nn.relu(layer_2)
+    layer_2 = tf.add(tf.matmul(do_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
     #layer_2 = tf.nn.sigmoid(layer_2)
-    layer_2 = tf.nn.softmax(layer_2)
+    do_2 = tf.nn.dropout(layer_2, keep_prob)
 #
-    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
+    layer_3 = tf.add(tf.matmul(do_2, weights['h3']), biases['b3'])
+    layer_3 = tf.nn.relu(layer_3)
     ##layer_3 = tf.nn.sigmoid(layer_3)
-    layer_3 = tf.nn.softmax(layer_3)
-    ## Output layer with linear activation
-    out_layer = tf.add(tf.matmul(layer_3, weights['out']), biases['out'])
+    do = tf.nn.dropout(layer_3, keep_prob)
+    out_layer = tf.add(tf.matmul(do, weights['out']), biases['out'])
     return out_layer
 
 # training
 def train(args):
 
     #csvからdataframeへ
-    df = pd.read_csv( "./input/train.csv", index_col="id" )
-
-    ngcol = ['id','target']
-    usecolumns = [ cname for cname in df.columns if cname not in ngcol ]
-
-    # 説明変数
-    x_np = np.array(df[usecolumns])
+    df = pd.read_csv( "./input/train.csv" )
 
     # 目的変数を2次元化
-    #df['target'].where(df['target'] == 0, 'no')
-    #df['target'].where(df['target'] == 1, 'yes')
-    #dd =  pd.get_dummies(df['target'])
-    #d = dd.to_dict('record')
-    #vectorizer = DictVectorizer(sparse=False)
-    #y_np = vectorizer.fit_transform(d)
+    #y = df['target']
+    #y1 = abs(-1+y)
+    #y_np = np.array(pd.concat([y, y1], axis=1))
 
     y_np = []
     for i in df.index:
         yclass = [0.0,0.0]
         yclass[df['target'][i]] = 1.0
         y_np.append(yclass)
-    
+
+    df = df.drop(['id','target'],axis=1)
+    binarizerandscaler = FeatureBinarizatorAndScaler()
+    binarizerandscaler.fit(df)
+    x_np = binarizerandscaler.transform(df)
+    n_input = x_np.shape[1]
+
     [x_train, x_test] = np.vsplit(x_np, [train_size]) # 入力データを訓練データとテストデータに分ける
     [y_train, y_test] = np.vsplit(y_np, [train_size]) # ラベルを訓練データをテストデータに分ける
     
     print x_train[0:2]
     print y_train[0:2]
     
-    # Network Parameters
-    n_input = len(usecolumns)
     
     # tf Graph input
     x = tf.placeholder("float", [None, n_input])
     y = tf.placeholder("float", [None, n_classes])
+    keep_prob = tf.placeholder("float")
 
-    #Y_ = []
-    #label =  tf.placeholder("float",[None,n_classes])
-    
 
     # Construct model
-    pred = multilayer_perceptron(x, n_input)
+    pred = multilayer_perceptron(x, n_input, keep_prob)
     #pred = model_softmax(x,n_input)
     y_ = tf.nn.softmax(pred)
 
-    
     global_step = tf.Variable(0, name="global_step", trainable=False)
     # Define loss and optimizer
     v13 = False
@@ -131,8 +126,8 @@ def train(args):
         cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
     #cost = tf.reduce_mean(tf.square(pred - y))
     #cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost,global_step)
-    #optimizer = tf.train.MomentumOptimizer(0.01,0.97).minimize(cost)
+    #optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost,global_step)
+    optimizer = tf.train.MomentumOptimizer(0.002,0.5).minimize(cost)
     #optimizer = tf.train.GradientDescentOptimizer(1e-1).minimize(cost)
 
 
@@ -182,7 +177,8 @@ def train(args):
                 y_train_batch = y_train[ind]
                 # Run optimization op (backprop) and cost op (to get loss value)
                 _, c = sess.run([optimizer, cost], feed_dict={x: x_train_batch,
-                                                              y: y_train_batch})
+                                                              y: y_train_batch,
+                                                              keep_prob: prob})
                 # Compute average loss
                 avg_cost += c
 
@@ -196,7 +192,8 @@ def train(args):
             current_step = tf.train.global_step(sess, global_step)
             saver.save(sess, './models/model', global_step=current_step)
             summary_str = sess.run(summary_op, feed_dict={x: x_train_batch,
-                                                          y: y_train_batch})
+                                                          y: y_train_batch,
+                                                          keep_prob:prob})
             summary_writer.add_summary(summary_str, epoch)
             summary_writer.flush() 
 
@@ -210,7 +207,7 @@ def train(args):
             tf.summary.scalar("Accuracy on Train", accuracy)
         else:
             tf.scalar_summary("Accuracy on Train", accuracy)
-        print "Accuracy:", accuracy.eval({x: x_test, y: y_test})
+        print "Accuracy:", accuracy.eval({x: x_test, y: y_test, keep_prob:prob})
         #print "auc %f:" % auc.eval({x: x_test, y: y_test}) 
         
         saver.save(sess, modelsfile)
@@ -223,21 +220,22 @@ def test(args):
     #csvからdataframeへ
     df = pd.read_csv( "./input/test.csv" )
 
-    # 説明変数
-    ngcol = ['id']
-    usecolumns = [ cname for cname in df.columns if cname not in ngcol ]
-    x_np = np.array(df[usecolumns])
-
+    ids = np.array(df['id'])
+    df = df.drop(['id'],axis=1)
+    binarizerandscaler = FeatureBinarizatorAndScaler()
+    binarizerandscaler.fit(df)
+    x_np = binarizerandscaler.transform(df)
 
     # Network Parameters
-    n_input = len(usecolumns)
+    n_input = x_np.shape[1]
     
     # tf Graph input
     x = tf.placeholder("float", [None, n_input])
     y = tf.placeholder("float", [None, n_classes])
+    keep_prob = tf.placeholder("float")
 
     # Construct model
-    pred = multilayer_perceptron(x,n_input)
+    pred = multilayer_perceptron(x,n_input,keep_prob)
     #pred = model_softmax(x,n_input)
     y_ = tf.nn.softmax(pred)
     
@@ -251,10 +249,10 @@ def test(args):
             fo.writerow(['id','target'])
                 
             #predicted = sess.run(pred, feed_dict={x:x_np})
-            predicted = sess.run(y_, feed_dict={x:x_np})
+            predicted = sess.run(y_, feed_dict={x:x_np, keep_prob:prob})
             print predicted
             for i, p in enumerate(predicted):
-                fo.writerow([df['id'][i],'%1.4f'%p[1]])
+                fo.writerow([ids[i],'%1.4f'%p[1]])
         
     return
 
